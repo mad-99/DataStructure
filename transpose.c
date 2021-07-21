5bc7f7c8ddcb9ed2750d8e3891bdf76ae4b75e39
mad-99 <72245519+mad-99@users.noreply.github.com>
2021-07-22 00:29:40 +0530

    Add files via upload

---
+++ transpose.c
@@ -0,0 +1,47 @@
+#include<stdio.h>
+void inputmatrix(int [][10],int,int);
+void outputmatrix(int [][10],int,int);
+void tranmatrix(int [][10],int);
+void main(){
+int mat1[10][10],row,col;
+	printf("enter the value of row...");
+	scanf("%d",&row);
+	printf("enter the value of col...");
+	scanf("%d",&col);
+	printf("enter the first matrix \n");
+	inputmatrix(mat1,row,col);
+	tranmatrix(mat1,row,col);
+	printf("transpose of matrix\n");
+	outputmatrix(mat1,row);	
+}
+void inputmatrix(int x[][10],int r,int c){
+	int i,j;
+	for(i=0;i<r;i++){
+		for(j=0;j<c;j++){
+			printf("enter the value %d,%d ",i,j);
+			scanf("%d",&x[i][j]);
+		}
+	}
+}
+void outputmatrix(int x[][10],int r,int c){
+	int i,j;
+	for(i=0;i<r;i++){
+		for(j=0;j<c;j++){
+			printf("%d ",x[i][j]);
+		}
+		printf("\n");
+	}
+}
+void tranmatrix(int x[][10],int r){
+	int i,j,temp=0;
+	for(i=0;i<r;i++){
+		for(j=0;j<i;j++){
+			if (i!=j)
+			{
+				temp=x[i][j];
+				x[i][j]=x[j][i];
+				x[j][i]=temp;
+			}
+		}
+	}
+}
